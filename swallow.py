e4586d9b36762cdee2362efb0b8e5945e62dcb3f
Maksim Melnikov <approximatenumber@gmail.com>
2016-12-27 17:08:07 +0300

    Added easy repairing tool for data correction (see weewx repo)

--- swallow.py
+++ swallow.py
@@ -6,8 +6,9 @@ import syslog
 import time
 import struct
 
-
 import weewx.drivers
+
+from swallow_lib import check_and_repair
 
 DRIVER_NAME = 'Swallow'
 DRIVER_VERSION = '1.0'
@@ -37,35 +38,35 @@ def logerr(msg):
 
 class SwallowDriver(weewx.drivers.AbstractDevice):
     def __init__(self, **stn_dict):
-	self.port = stn_dict.get('port', DEFAULT_PORT)
-	self.loop_interval = float(stn_dict.get('loop_interval', 60.0))
-	loginf('driver version is %s ' % DRIVER_VERSION)
-	loginf('using serial port %s ' % self.port)
+        self.port = stn_dict.get('port', DEFAULT_PORT)
+        self.loop_interval = float(stn_dict.get('loop_interval', 60.0))
+        loginf('driver version is %s ' % DRIVER_VERSION)
+        loginf('using serial port %s ' % self.port)
 
-	global DEBUG_READ
-	DEBUG_READ = int(stn_dict.get('debug_read', DEBUG_READ))
-	self.station = Station(self.port)
-	self.station.open()	
+        global DEBUG_READ
+        DEBUG_READ = int(stn_dict.get('debug_read', DEBUG_READ))
+        self.station = Station(self.port)
+        self.station.open() 
 
     def closePort(self):
-	if self.station is not None:
-	    self.station.close()
-	    self.station = None
+        if self.station is not None:
+            self.station.close()
+            self.station = None
 
     @property
     def hardware_name(self):
-	return 'Swallow'
+        return 'Swallow'
 
     def genLoopPackets(self):
-	while True:
-	    packet = {'dateTime': int(time.time() + 0.5),
-		      'usUnits': weewx.METRIC}
-	    readings = self.station.get_readings()
-	    data = self.station.parse_readings(readings)
+        while True:
+            packet = {'dateTime': int(time.time() + 0.5),
+                      'usUnits': weewx.METRIC}
+            readings = self.station.get_readings()
+            data = check_and_repair(self.station.parse_readings(readings))
             Station.print_data(data)
-	    packet.update(data)
-	    time.sleep(self.loop_interval)
-	    yield packet
+            packet.update(data)
+            time.sleep(self.loop_interval)
+            yield packet
 
 class Station(object):
     START = 5
@@ -74,58 +75,58 @@ class Station(object):
     MAX_HUMI = 100.0
     MIN_HUMI = 0.0
     def __init__(self, port):
-	self.port = port
-	self.baudrate = BAUDRATE
-	self.timeout = 3
-	self.serial_port = None
-	self.last_rain = 0.0
+        self.port = port
+        self.baudrate = BAUDRATE
+        self.timeout = 3
+        self.serial_port = None
+        self.last_rain = 0.0
         self.last_geiger = 0
 
     def __enter__(self):
-	self.open()
-	return self
+        self.open()
+        return self
 
     def __exit__(self, _, value, traceback):
-	self.close()
+        self.close()
 
     def open(self):
-	self.serial_port = serial.Serial(self.port, self.baudrate,
-					 timeout=self.timeout)
-											  
+        self.serial_port = serial.Serial(self.port, self.baudrate,
+                                         timeout=self.timeout)
+                                              
     def close(self):
-	if self.serial_port is not None:
-	    self.serial_port.close()
-	    self.serial_port = None
+        if self.serial_port is not None:
+            self.serial_port.close()
+            self.serial_port = None
 
     def get_readings(self):
-	data_left = 0
-	while data_left < PACKET_BYTE_COUNT:
-	    self.serial_port.flushInput()
-	    self.serial_port.write(b'\xAA\xBB\x00\x02\x2A\x6E\xFE')
-	    time.sleep(10)
-	    data_left = self.serial_port.inWaiting()
-	    
+        data_left = 0
+        while data_left < PACKET_BYTE_COUNT:
+            self.serial_port.flushInput()
+            self.serial_port.write(b'\xAA\xBB\x00\x02\x2A\x6E\xFE')
+            time.sleep(10)
+            data_left = self.serial_port.inWaiting()
+        
         received_bytes = self.serial_port.read(PACKET_BYTE_COUNT)
-	return [str(val) for val in received_bytes[self.START:self.END]]
+        return [str(val) for val in received_bytes[self.START:self.END]]
 
     def parse_readings(self, raw):
-	data = dict()
-	raw.reverse()
+        data = dict()
+        raw.reverse()
 
-	data['windDir'] = Station.hex_to_int(''.join(raw[40:42]))
-	data['outTemp'] = Station.hex_to_float(''.join(raw[36:40]))
-	data['pressure'] = Station.hex_to_float(''.join(raw[32:36]))
-	data['long_term_rain'] = Station.hex_to_float(''.join(raw[28:32]))
-	data['windSpeed'] = Station.hex_to_float(''.join(raw[24:28]))
-	data['outHumidity'] = Station.get_humi(Station.hex_to_float(''.join(raw[20:24])))
-	data['long_term_geiger'] = Station.hex_to_int(''.join(raw[16:20]))
-	data['illumination'] = Station.hex_to_float(''.join(raw[12:16]))
-	data['inTemp'] = Station.hex_to_float(''.join(raw[8:12]))
-	data['maxWind'] = Station.hex_to_float(''.join(raw[4:8]))
-	data['downfall'] = bool(Station.hex_to_int(''.join(raw[0:4])))
+        data['windDir'] = Station.hex_to_int(''.join(raw[40:42]))
+        data['outTemp'] = Station.hex_to_float(''.join(raw[36:40]))
+        data['pressure'] = Station.hex_to_float(''.join(raw[32:36]))
+        data['long_term_rain'] = Station.hex_to_float(''.join(raw[28:32]))
+        data['windSpeed'] = Station.hex_to_float(''.join(raw[24:28]))
+        data['outHumidity'] = Station.get_humi(Station.hex_to_float(''.join(raw[20:24])))
+        data['long_term_geiger'] = Station.hex_to_int(''.join(raw[16:20]))
+        data['illumination'] = Station.hex_to_float(''.join(raw[12:16]))
+        data['inTemp'] = Station.hex_to_float(''.join(raw[8:12]))
+        data['maxWind'] = Station.hex_to_float(''.join(raw[4:8]))
+        data['downfall'] = bool(Station.hex_to_int(''.join(raw[0:4])))
         data['deltarain'] = self.get_delta_rain(data['long_term_rain'])
         data['geiger'] = self.get_delta_geiger(data['long_term_geiger'])
-	return data
+        return data
 
     @staticmethod
     def hex_to_float(val):
@@ -137,9 +138,9 @@ class Station(object):
 
     @staticmethod
     def get_humi(val):
-        if val > Station.MAX_HUMI: return MAX_HUMI
-        if val < Station.MIN_HUMI: return MIN_HUMI
-	return val
+        if val > Station.MAX_HUMI: return Station.MAX_HUMI
+        if val < Station.MIN_HUMI: return Station.MIN_HUMI
+        return val
 
     @staticmethod
     def print_data(data):
